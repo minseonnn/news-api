28831dbddafdf95304e904594e0f6caccdff4a33
minseonnn <119590961+minseonnn@users.noreply.github.com>
2024-01-02 18:09:36 +0900

    Modals  Create/Delete
    기사 이미지나 타이틀 클릭 시 모달 생성/ X 클릭시 모달 삭제

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const newsBox = document.querySelector(".news_box");
 const newsList =document.querySelector(".news_list");
 
 const modal_wrap = document.querySelector('.modal_wrap');
-const modal_content = document.querySelector('modal_content');
-const modal_close = document.querySelector(".modal_close");
-const goToNews = document.querySelector('.goToNews');
 
 // 날짜 오늘로 변경하기
 let newsDate = new Date().toISOString().substring(0, 10);
@@ -40,67 +37,91 @@ function handleForm(event) {
     console.log(data.articles);
     newsList.innerHTML = '';
     // console.log(article_list);
-    data.articles.forEach(article => {
-      // ul 안에 그려 줄 new item 
-      let li = document.createElement("li");
-      li.className = "news_li";
-      newsList.appendChild(li);
-      //기사 이미지 링크
-      let thumbnail_box = document.createElement('div');
-      thumbnail_box.className = "thumbnail_box"
-      thumbnail_box.addEventListener("click", modalOpen);
-      li.appendChild(thumbnail_box);
-      //기사 제목 링크
-      let title_box = document.createElement('div');
-      title_box.className = "title_box"
-      li.appendChild(title_box);
-      // 뉴스 썸네일
-      let thumbnail = document.createElement('div');
-      thumbnail.className = "news_thumbnail";
-      let thumbnail_img = document.createElement('img');
-      thumbnail_img.className = "thumbnail_img";
-      thumbnail_box.appendChild(thumbnail);
-      thumbnail.appendChild(thumbnail_img);
-      if(article.urlToImage !== null) {
-        thumbnail_img.setAttribute('src', article.urlToImage);
-      } else if (article.urlToImage == null) {
-        const imgPath = "./img/no_img.jpg";
-        thumbnail_img.setAttribute('src', imgPath);
-      }
-      //뉴스 타이틀
-      let title = document.createElement('div');
-      title.innerText = article.title;
-      title.className = "news_title";
-      title_box.appendChild(title)
-      title.addEventListener("click", modalOpen);
-      //뉴스 내용(description)
-      let content = document.createElement('div');
-      content.innerText = article.description;
-      title_box.appendChild(content);
-
-      //모달창 
-      let mNews_title = document.querySelector('.mNews_title');
-      mNews_title.innerText = article.title;
-
-      let mNews_content = document.querySelector('.mNews_content');
-      mNews_content.innerText = article.description;
-      
-      let mNews_author = document.querySelector('.mNews_author');
-      mNews_author.innerText = article.author;
-
-      goToNews.addEventListener("click", function(){newsView(article.url)} );
+    
+    const arrA = data.articles.map(article => {
+    const li = createListElement(article);
+    li.addEventListener('click', () => createModal(article))
+    return li
     });
+    console.log(arrA);
+    arrA.forEach(element => newsList.appendChild(element));
   }
   ).catch(error => console.error(error.message));
 ;
 };
 
-function newsView(link) {
-  location.href = link
+function createListElement (article) {
+  // ul 안에 그려 줄 new item 
+  let li = createElement("li", "news_li");
+  
+  //기사 이미지 링크
+  let thumbnail_box = createElement('div','thumbnail_box');
+  thumbnail_box.addEventListener("click", modalOpen);
+  li.appendChild(thumbnail_box);
+  
+  let thumbnail_img = createElement('img','thumbnail_img');
+  if (article.urlToImage !== null) {
+    thumbnail_img.setAttribute('src', article.urlToImage);
+  } else if (article.urlToImage == null) {
+    const imgPath = "./img/no_img.jpg";
+    thumbnail_img.setAttribute('src', imgPath);
+  }
+  thumbnail_box.appendChild(thumbnail_img);
+  
+  //기사 제목 링크
+  let title_box = createElement('div','title_box');
+  li.appendChild(title_box);
+  //뉴스 타이틀
+  let title = createElement('h2', "news_title");
+  title.innerText = article.title;
+  title_box.appendChild(title)
+  title.addEventListener("click", modalOpen);
+
+  //뉴스 내용(description)
+  let content = createElement('p');
+  content.innerText = article.description;
+  title_box.appendChild(content);
+
+  return li
 }
 
+function createModal (article) {
+//모달창 
+  let modal_content = createElement('div','modal_content');
+  let mNews_title = createElement('h3','mNews_title');
+  mNews_title.innerText = article.title;
+  let mNews_author = createElement('p','mNews_author');
+  mNews_author.innerText = article.author;
+  let mNews_content = createElement('p','mNews_content');
+  mNews_content.innerText = article.description.slice(0,100) + "...";
+  let mNews_view = createElement('a','mNews_view');
+  mNews_view.href = article.url
+  mNews_view.innerText = 'view';
+  let modal_close = createElement('button','modal_close');
+  modal_close.innerText = 'X';
+  modal_close.addEventListener("click", modalClose);
+
+  modal_wrap.appendChild(modal_content);
+  modal_content.appendChild(modal_close);
+  modal_content.appendChild(mNews_title);
+  modal_content.appendChild(mNews_author);
+  modal_content.appendChild(mNews_content);
+  modal_content.appendChild(mNews_view);
+  
+}
+
+// {className, onClick, src, innerText}
+function createElement(elementTag, className) {
+  const element = document.createElement(elementTag);
+  element.classList.add(className)
+  return element
+}
+
+
 function modalClose(event) {
+  let modal_content = document.querySelector(".modal_content");
   if(event.target.className === "modal_close"){
+  modal_wrap.removeChild(modal_content);
   modal_wrap.style.display = 'none';
   }
 }
@@ -133,7 +154,5 @@ const callNextPage = () =>{
 } 
 observeIntersection(newsList, callNextPage);
 
-
-modal_close.addEventListener("click", modalClose);
-
 searchForm.addEventListener("submit", handleForm);
+
